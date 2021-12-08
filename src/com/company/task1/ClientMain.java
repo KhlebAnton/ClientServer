aed78e963a41e5f62c0f3291fba172a9a9c58130
Anton Khlebnikov <toni.farsh@icloud.com>
2021-12-08 15:11:07 +0500

    Tasks

---
+++ src/com/company/task1/ClientMain.java
@@ -0,0 +1,24 @@
+package com.company.task1;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClientMain {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("127.0.0.1", 55334);
+        try (BufferedReader in = new BufferedReader(
+                new InputStreamReader(socket.getInputStream()));
+             PrintWriter out = new PrintWriter(
+                     new OutputStreamWriter(socket.getOutputStream()), true);
+             Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                System.out.println("Введите целое положительное число для подсчета N-го члена ряда Фибоначчи(для выхода: end): ");
+                String msg = scanner.nextLine();
+                if ("end".equals(msg)) break;
+                out.println(msg);
+                System.out.println("Ответ сервера : " + in.readLine());
+            }
+        }
+    }
+}
